212ead775fc1f594581ba078e740201b7272a264
Jiacheng-Yang-42 <owenyjc981210@gmail.com>
2023-09-30 20:50:49 +0800

    Assessment 2

--- Assessment 2 project/2-copy/StudentScoreStatistics.java
+++ Assessment 2 project/2-copy/StudentScoreStatistics.java
@@ -10,13 +10,22 @@ import java.io.*;
 public class StudentScoreStatistics{
     public static void main(String[] args){
     try (Scanner scanner = new Scanner(new File("prog5001_students_grade_2022.csv"))){
-            String unitName = null;
-            int studentCount = 0;
+            String unitName = parts[0];
+            int lineCount = 0;
         while (scanner.hasNextLine()) {
-                String input = scanner.nextLine();
-                String[] parts = input.split(",");
-        }
-    String studentName = parts[0] + " " + parts[1]; 
+        lineCount++;         
+        String input = scanner.nextLine();
+        String[] parts = input.split(",");
+        String studentName = parts[0] + " " + parts[1];
+        String studentID = parts[2];
+        double assigment1 = Double.parseDouble(parts[3]);
+        double assigment2 = Double.parseDouble(parts[4]);
+        double assigment3 = Double.parseDouble(parts[5]);
+        double totalMark = assigment1 + assigment2 + assigment3;
+        System.out.println("Student " + (lineCount - 2) + "Name: " + studentName);
+        System.out.println("Student " + (lineCount - 2) + "ID: " + studentID);
+          }
+        
     }
 }
 }
